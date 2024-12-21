a59d4e980633e41a97cfc8c67b11ebbdbe2da0a8
Fedelya <100048385+Fedelya@users.noreply.github.com>
2024-12-21 13:27:16 +0700

    config security

--- src/main/java/vn/edu/iuh/fit/trananhtien/backend/models/Address.java
+++ src/main/java/vn/edu/iuh/fit/trananhtien/backend/models/Address.java
@@ -1,11 +1,15 @@
 package vn.edu.iuh.fit.trananhtien.backend.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "address")
 public class Address {
--- src/main/java/vn/edu/iuh/fit/trananhtien/frontend/controllers/AddressController.java
+++ src/main/java/vn/edu/iuh/fit/trananhtien/frontend/controllers/AddressController.java
@@ -1,13 +1,29 @@
 package vn.edu.iuh.fit.trananhtien.frontend.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vn.edu.iuh.fit.trananhtien.backend.models.Address;
+import vn.edu.iuh.fit.trananhtien.backend.repositories.AddressRepository;
+
+import java.util.List;
+
 
 @RestController
 public class AddressController {
 
+    @Autowired
+    private AddressRepository addressRepository;
+
     @GetMapping("/index")
-    public String welcome(){
-        return "";
+    public String welcome(HttpServletRequest request){
+        return "" + request.getSession().getId();
+    }
+
+    @GetMapping("/address")
+    public List<Address> address(Model model){
+        return addressRepository.findAll();
     }
 }
